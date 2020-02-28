6379288b8e30d9b6e3496f476f2ba81200a5a3e8
dnkudale <dnkudale97@gmail.com>
2020-02-28 11:51:43 +0530

    To check the Code quality

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,8 @@ describe('String', function () {
     describe('#indexOf', function () {
         it('should return -1 when the substring is not present', function () {
             var myString = 'test';
+            var Name ='Dnyanesh';
+            var SurName = 'Kudale';
 
             assert.equal(-1, myString.indexOf('x'));
             assert.equal(-1, myString.indexOf('y'));
